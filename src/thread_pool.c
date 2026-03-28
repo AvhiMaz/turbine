6c1729c5b7c911b3de9071e3b1e19cf8875f4288
AvhiMaz <avhimazumder5@outlook.com>
2026-03-28 22:53:24 +0530

    feat: add worker
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- src/thread_pool.c
+++ src/thread_pool.c
@@ -1,8 +1,48 @@
 #include "thread_pool.h"
 #include "defines.h"
 #include "shred.h"
+#include <stdio.h>
 
-void worker() {}
+void *worker(void *args) {
+    ThreadPool *tp = (ThreadPool *)args;
+
+    while (1) {
+        pthread_mutex_lock(&tp->mutex);
+
+        while (tp->count == 0 && !tp->shutdown) {
+            pthread_cond_wait(&tp->cond, &tp->mutex);
+        }
+        if (tp->shutdown) {
+            pthread_mutex_unlock(&tp->mutex);
+            return NULL;
+        }
+
+        Shred *s = tp->queue[tp->head];
+
+        tp->head = (tp->head + 1) % MAX_QUEUE;
+
+        tp->count--;
+
+        pthread_mutex_unlock(&tp->mutex);
+
+        int ok = validate_shred(s);
+
+        if (ok == 1) {
+            printf("shred[%d] OK", s->index);
+        } else {
+            printf("shred[%d] CORRUPT", s->index);
+        }
+
+        pthread_mutex_lock(&tp->mutex);
+
+        tp->pending--;
+
+        if (tp->pending == 0) {
+            pthread_cond_signal(&tp->done_cond);
+        }
+        pthread_mutex_unlock(&tp->mutex);
+    }
+}
 
 void tp_init(ThreadPool *tp) {
     tp->head = 0;
