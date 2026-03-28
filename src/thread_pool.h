8d3a4a75e9984e81c30a8067cc5696d901e015c5
AvhiMaz <avhimazumder5@outlook.com>
2026-03-28 20:48:34 +0530

    chore: add thread pool struct
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- src/thread_pool.h
+++ src/thread_pool.h
@@ -1,7 +1,19 @@
+#ifndef THREAD_POOL_H
+#define THREAD_POOL_H
+
 #include "shred.h"
 #include <pthread.h>
 
 typedef struct {
-    pthread_t thread[NUMBER_OF_THREADS];
-    Shred    *queue[MAX_QUEUE];
+    pthread_t       thread[NUMBER_OF_THREADS];
+    Shred          *queue[MAX_QUEUE];
+    int             head, tail, count;
+    pthread_mutex_t mutex;
+    pthread_cond_t  cond;
+    pthread_cond_t  done_cond;
+    int             pending;
+    int             shutdown;
+
 } ThreadPool;
+
+#endif
