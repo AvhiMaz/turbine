a9e1708b4174200eb8c1d0e738ddc4eeb5ea955f
AvhiMaz <avhimazumder5@outlook.com>
2026-03-28 22:07:19 +0530

    feat: impl `tp_submit` fn
    this will submit the shred in a queue
    
    Signed-of-by: AvhiMaz <avhimazumder5@outlook.com>

--- src/thread_pool.c
+++ src/thread_pool.c
@@ -1,4 +1,6 @@
 #include "thread_pool.h"
+#include "defines.h"
+#include "shred.h"
 
 void tp_init(ThreadPool *tp) {
     tp->head = 0;
@@ -9,4 +11,14 @@ void tp_init(ThreadPool *tp) {
     pthread_mutex_init(&tp->mutex, NULL);
     pthread_cond_init(&tp->cond, NULL);
     pthread_cond_init(&tp->done_cond, NULL);
+}
+
+void tp_submit(ThreadPool *tp, Shred *s) {
+    pthread_mutex_lock(&tp->mutex);
+    tp->queue[tp->tail] = s;
+    tp->tail = (tp->tail + 1) % MAX_QUEUE;
+    tp->count++;
+    tp->pending++;
+    pthread_cond_signal(&tp->cond);
+    pthread_mutex_unlock(&tp->mutex);
 }
